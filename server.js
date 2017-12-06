3d749c24653158977d9f8e0b5e9619b0099c0d8d
Sam Grumley <grumsamu@gmail.com>
2017-12-06 21:41:43 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require ('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log');
+    }
+  })
+  next();
+});
+
+//uncomment maintenance
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+//routes, handling
+// app.get('/', (req, res) => {
+//   // res.send('<h1>Hello</h1>');
+//   res.send({
+//     name: 'Jesse',
+//     Likes: [
+//       'boys',
+//       'dicks'
+//     ]
+//   });
+// });
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home',
+    welcomeMessage: 'Hello there'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'unable to handle request'
+  });
+});
+
+
+
+app.listen(3000, () =>{
+  console.log('server is up on port 3000');
+});
